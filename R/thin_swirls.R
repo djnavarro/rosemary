d8d2705ac5af508c17e0a5e55ae44649d4ff0827
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-23 00:11:14 +1100

    adds thin_swirls()

---
+++ R/thin_swirls.R
@@ -0,0 +1,46 @@
+#' @rdname rosemary
+#' @export
+thin_swirls <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "thin_swirls.png")
+
+  set.seed(46)
+
+  dat <- jasmines::scene_sticks(3, 30) %>%
+    jasmines::unfold_meander(
+      output1 = "space",
+      output2 = "order"
+    ) %>%
+    dplyr::mutate(
+      base_x = x,
+      base_y = y,
+      len = sqrt(x^2 + y^2) * .8
+    ) %>%
+    jasmines::unfold_tempest(
+      output1 = "time",
+      output2 = "junk",
+      iterations = 1
+    )
+
+  dat <- dat %>%
+    dplyr::mutate(
+      x = dplyr::case_when(
+        time == 1 ~ base_x,
+        time == 2 ~ (1-len) * base_x + len * x
+      ),
+      y = dplyr::case_when(
+        time == 1 ~ base_y,
+        time == 2 ~ (1-len) * base_y + len * y
+      )
+    )
+
+  dat %>% jasmines::style_ribbon(
+    alpha_init = .8,
+    size = .5,
+    palette = jasmines::palette_named("magma")
+  ) %>% jasmines::export_image(file)
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+}
