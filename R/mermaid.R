d1135229462959d76a53c0852501556e0c7f83db
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-22 22:34:19 +1100

    adds mermaid()

---
+++ R/mermaid.R
@@ -0,0 +1,22 @@
+#' @rdname rosemary
+#' @export
+mermaid <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "mermaid.png")
+
+  set.seed(1)
+  jasmines::scene_delaunay(6, 500) %>%
+    jasmines::unfold_slice() %>%
+    jasmines::style_ribbon(
+      palette = grDevices::gray.colors,
+      seed_col = "#ffffffaa",
+      alpha_init = .1,
+      background = "white"
+    ) %>% jasmines::export_image(file)
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+}
+
+
