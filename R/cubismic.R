a3f3b7103a5fa3415a783e9fa7d19cbebbf27843
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-22 21:36:16 +1100

    adds cubismic()

---
+++ R/cubismic.R
@@ -0,0 +1,44 @@
+#' @rdname rosemary
+#' @export
+cubismic <- function(dir = NULL, version = 0, ...) {
+
+  dir <- check_dir(dir)
+
+  if(version %in% c(0,1)) {
+
+    file <- file.path(dir, "cubismic_light.png")
+    set.seed(435)
+    jasmines::scene_delaunay(6, 500) %>%
+      jasmines::unfold_slice() %>%
+      jasmines::style_ribbon(
+        palette = jasmines::palette_named("bilbao"),
+        seed_col = "#ffffffaa",
+        alpha_init = .1,
+        background = "grey10"
+      ) %>% jasmines::export_image(file)
+    cat("image written to:", file, "\n")
+
+  }
+
+  if(version %in% c(0,2)) {
+
+    file <- file.path(dir, "cubismic_rainbow.png")
+    set.seed(435)
+    jasmines::scene_delaunay(6, 500) %>%
+      jasmines::unfold_slice() %>%
+      jasmines::style_ribbon(
+        palette = grDevices::rainbow,
+        seed_col = "#ffffffaa",
+        alpha_init = .1,
+        background = "#444466"
+      ) %>% jasmines::export_image(file)
+    cat("image written to:", file, "\n")
+
+  }
+
+  return(invisible(NULL))
+}
+
+
+
+
