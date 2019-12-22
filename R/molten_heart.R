11e771d8b8ca8973a2df26bad4de8e87a4b39e98
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-22 22:48:07 +1100

    adds molten_heart()

---
+++ R/molten_heart.R
@@ -0,0 +1,67 @@
+#' @rdname rosemary
+#' @export
+molten_heart <- function(dir = NULL, version = 0, ...) {
+
+  dir <- check_dir(dir)
+
+  molten_heart <- function(palette, shuffle, file) {
+
+    dat <- jasmines::entity_heart(1000) %>%
+      dplyr::mutate(
+        x = x * 2,
+        y = y * 2
+      ) %>%
+      jasmines::unfold_tempest(
+        iterations = 40,
+        scale = .01,
+        seed = 125
+      )
+
+    if(shuffle == FALSE) {
+      dat <- dat %>% dplyr::mutate(order = time)
+    }
+
+    dat %>%
+      jasmines::style_ribbon(
+        alpha_init = .8,
+        alpha_decay = .1,
+        size = 1,
+        palette = palette
+      ) %>%
+      jasmines::export_image(file)
+
+    cat("image written to:", file, "\n")
+  }
+
+  if(version %in% c(0,1)) {
+
+    set.seed(125)
+    molten_heart(
+      palette = jasmines::palette_named("berlin"),
+      shuffle = TRUE,
+      file = file.path(dir, "molten_heart_shuffled.png")
+    )
+  }
+
+  if(version %in% c(0,2)) {
+    set.seed(125)
+    molten_heart(
+      palette = jasmines::palette_manual("grey"),
+      shuffle = FALSE,
+      file = file.path(dir, "molten_heart_grey.png")
+    )
+  }
+
+  if(version %in% c(0,3)) {
+    set.seed(125)
+    molten_heart(
+      palette = grDevices::rainbow,
+      shuffle = FALSE,
+      file = file.path(dir, "molten_heart_rainbow.png")
+    )
+  }
+
+  return(invisible(NULL))
+}
+
+
