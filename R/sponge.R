8268e77453efe9f104deb265c7381736af122bde
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-22 23:51:21 +1100

    adds sponge()

---
+++ R/sponge.R
@@ -0,0 +1,43 @@
+#' @rdname rosemary
+#' @export
+sponge <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "sponge.png")
+  set.seed(2)
+
+  darken <- function(palette, scale = .5) {
+    function(n = 50, alpha = 1) {
+      rgbvec <- purrr::lift_dv(grDevices::rgb)
+      cols <- palette(n, alpha)
+      cols <- grDevices::col2rgb(cols)
+      cols <- cols * scale / 256
+      cols <- as.data.frame(cols)
+      cols <- purrr::map_chr(cols, rgbvec)
+      return(unname(cols))
+    }
+  }
+
+  jasmines::scene_delaunay(5, grain = 200) %>%
+    dplyr::mutate(x = x * 15, y = y * 15) %>%
+    jasmines::unfold_loop(points = 200, radius = 2) %>%
+    dplyr::mutate(id = time) %>%
+    dplyr::select(-time) %>%
+    jasmines::unfold_tempest(
+      scale = .2,
+      iterations = 5,
+      seed = 23
+    ) %>%
+    dplyr::mutate(order = id) %>%
+    jasmines::style_ribbon(
+      background = "darkslategray",
+      burnin = 4,
+      type = "curve",
+      alpha_init = .05,
+      size = 2,
+      palette = darken(jasmines::palette_named("cork"), .5),
+    ) %>% jasmines::export_image(file)
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+}
