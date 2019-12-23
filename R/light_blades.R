b6047117294cc8e83459be5d0da99598596c04b3
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-23 12:43:24 +1100

    adds light_blades()

---
+++ R/light_blades.R
@@ -0,0 +1,32 @@
+#' @rdname rosemary
+#' @export
+light_blades <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "light_blades.png")
+  set.seed(1)
+
+  jasmines::scene_grid(
+    xpos = c(-1, 1) * 4,
+    ypos = c(-1, 1) * 4,
+    entity = "gaussian",
+    grain = 2000
+  ) %>%
+    jasmines::unfold_slice(iterations=15, seed = 1) %>%
+    dplyr::mutate(order = dplyr::case_when(
+      id == 4 ~ (16 - order),
+      id == 1 ~ 1,
+      TRUE ~ (order + 5)
+    )) %>%
+    jasmines::style_ribbon(
+      palette = jasmines::palette_named("acton"),
+      background = "lightblue",
+      alpha_init = .5,
+      alpha_decay = .1,
+      size = .75
+    ) %>%
+    jasmines::export_image(file)
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+}
