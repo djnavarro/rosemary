84f85beac85988d47a533b84cff39dd4dda2244b
Danielle Navarro <d.navarro@unsw.edu.au>
2020-01-06 11:11:35 +1100

    adds anguish()

---
+++ R/anguish.R
@@ -0,0 +1,21 @@
+
+#' @rdname rosemary
+#' @export
+anguish <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "anguish.png")
+
+  jasmines::use_seed(1) %>%
+    jasmines::scene_grid(entity = "heart", grain = 1000, size = .5) %>%
+    jasmines::unfold_slice(scatter = TRUE) %>%
+    jasmines::unfold_inside() %>%
+    dplyr::mutate(order = 1 + ind*(inside > 0)) %>%
+    jasmines::style_ribbon(jasmines::palette_adjust("blood", "wheat"), background = "wheat") %>%
+    jasmines::export_image(file,  xlim = c(0,.9), ylim = c(0,.9))
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+}
+
+
