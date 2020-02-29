74873fe530bb37395f66403f9d0ad0f10607aadf
Danielle Navarro <d.navarro@unsw.edu.au>
2020-02-29 18:55:33 +1100

    add stars_and_the_night_sky

---
+++ R/stars_and_the_night_sky.R
@@ -0,0 +1,28 @@
+#
+#' @rdname rosemary
+#' @export
+stars_and_the_night_sky <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "stars_and_the_night_sky.png")
+
+  jasmines::use_seed(1) %>%
+    jasmines::scene_bubbles(n = 6, grain = 5000) %>%
+    jasmines::unfold_warp(
+      iterations = 100,
+      scale = .01
+    ) %>%
+    jasmines::style_ribbon(
+      palette = jasmines::palette_named("magma"),
+      colour = "id",
+      alpha = c(.2, .01),
+      background = "wheat"
+    ) %>%
+    jasmines::export_image(
+      filename = file
+    )
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+
+}
