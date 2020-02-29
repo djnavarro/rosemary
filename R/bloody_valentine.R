a2a7f8bf1f17fd288ab4a70d2a8cabcaa13870c0
Danielle Navarro <d.navarro@unsw.edu.au>
2020-02-29 18:40:10 +1100

    adds bloody_valentine

---
+++ R/bloody_valentine.R
@@ -0,0 +1,27 @@
+#' @rdname rosemary
+#' @export
+bloody_valentine <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "bloody_valentine.png")
+
+  jasmines::use_seed(224) %>%
+    jasmines::entity_heart(10000) %>%
+    jasmines::unfold_tempest(iterations = 200, scale = .005) %>%
+    jasmines::unfold_inside() %>%
+    jasmines::unfold_worley() %>%
+    dplyr::mutate(order = 1 + order(order) * (inside > 0)) %>%
+    jasmines::style_ribbon(
+      palette = "blood",
+      background = "white"
+    ) %>%
+    jasmines::export_image(
+      filename = file,
+      xlim = c(.25,.8),
+      ylim = c(.25,.8)
+    )
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+
+}
