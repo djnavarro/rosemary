4c6accd415274063056937142002dde2fdfc6842
Danielle Navarro <d.navarro@unsw.edu.au>
2020-02-29 19:09:41 +1100

    add sometime_anxious and sometime_angry

---
+++ R/sometime_angry.R
@@ -0,0 +1,29 @@
+#
+
+#' @rdname rosemary
+#' @export
+sometime_angry <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "sometime_angry.png")
+
+  jasmines::use_seed(241) %>%
+    jasmines::scene_sticks(
+      grain = 1000,
+    ) %>%
+    jasmines::unfold_warp(
+      iterations = 200,
+      scale = .003
+    ) %>%
+    jasmines::style_ribbon(
+      palette = jasmines::palette_named("lajolla"),
+      colour = "ind",
+      alpha = c(.1, .01),
+      background = "violetred4"
+    ) %>%
+    jasmines::export_image(filename = file)
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+
+}
---
+++ R/sometime_anxious.R
@@ -0,0 +1,32 @@
+#
+
+#' @rdname rosemary
+#' @export
+sometime_anxious <- function(dir = NULL, ...) {
+
+  dir <- check_dir(dir)
+  file <- file.path(dir, "sometime_anxious.png")
+
+  jasmines::use_seed(240) %>%
+    jasmines::scene_grid(
+      x = c(-.5,2.5),
+      y = c(-.5,2.5),
+      grain = 10000,
+      entity = "heart"
+    ) %>%
+    jasmines::unfold_warp(
+      iterations = 200,
+      scale = .003
+    ) %>%
+    jasmines::style_ribbon(
+      palette = jasmines::palette_named("viridis"),
+      colour = "ind",
+      alpha = c(.1, .01),
+      background = "whitesmoke"
+    ) %>%
+    jasmines::export_image(filename = file)
+
+  cat("image written to:", file, "\n")
+  return(invisible(NULL))
+
+}
